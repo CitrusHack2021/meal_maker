dddf25c6cfe742c46f9e81cfcf1624581f95fe9b
MRyali <mayurryali29@gmail.com>
2021-04-10 13:34:33 -0700

    Combine GeoLocator and Google Maps

--- lib/main.dart
+++ lib/main.dart
@@ -198,10 +198,6 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  double longitude = 0;
-  double latitude = 0;
-
-
   /// Determine the current position of the device.
   ///
   /// When the location services are not enabled or permissions
@@ -246,57 +242,38 @@ class MapSampleState extends State<MapSample> {
 
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: FutureBuilder(
+        future: _determinePosition(),
+        builder: (context, AsyncSnapshot<Position> currLoc) {
+          double latitude = currLoc.data.latitude;
+          double longitude = currLoc.data.longitude;
+
+          return GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: CameraPosition (
+                //target: LatLng(latitude, longitude),
+                target: LatLng(33.97237, -117.327469), // hard code values
+                zoom: 14.4746,
+            ),
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          );
+        }
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
 
 
+/***********************************************/
 
+// Geolocator initial Code to test functionality
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+/***********************************************/
 
 /*
 void main() {
