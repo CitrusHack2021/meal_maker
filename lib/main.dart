087f22f8dd59fdf19d57c9ae7b31ab24b4b56cad
MRyali <mayurryali29@gmail.com>
2021-04-10 21:23:32 -0700

    Fixed Nearby Restaurant Finder

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 
-import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 
 import 'package:google_maps_webservice/directions.dart';
@@ -39,7 +38,7 @@ class MyApp extends StatelessWidget {
 class MapSample extends StatefulWidget {
   @override
   //State<MapSample> createState() => MapSampleState();
-State<StatefulWidget> createState() {
+  State<StatefulWidget> createState() {
     return MapSampleState();
   }
 }
@@ -86,74 +85,20 @@ class MapSampleState extends State<MapSample> {
     // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
   }
-  /*
-  // Adds Marker to google Map
-  Set<Marker> _markers = HashSet<Marker>();
-  GoogleMapController _googleMapController;
-  BitmapDescriptor _markerIcon;
-
-  int _markerIdCounter = 1;
-
-  bool _isMarker = true;
-
-  // This function is to change the marker icon
-  void _setMarkerIcon() async {
-    _markerIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(), 'assets/farm.png');
-  }
-
-  // Set Markers to the map
-  void _setMarkers(LatLng point) {
-    final String markerIdVal = 'marker_id_$_markerIdCounter';
-    _markerIdCounter++;
-    setState(() {
-      print(
-          'Marker | Latitude: ${point.latitude}  Longitude: ${point.longitude}');
-      _markers.add(
-        Marker(
-          markerId: MarkerId(markerIdVal),
-          position: point,
-        ),
-      );
-    });
-  }*/
-
-  /*
-
-  final places = new GoogleMapsPlaces(apiKey: "<API_KEY>");
-  final places = new GoogleMapsPlaces(apiKey: "<API_KEY>", httpClient: new BrowserClient());
-  final places = new GoogleMapsPlaces(baseUrl: "http://myProxy.com");
-
-  PlacesSearchResponse response = await places.searchNearbyWithRadius(new Location(31.0424, 42.421), 500);
-  PlacesSearchResponse response = await places.searchNearbyWithRankby(new Location(31.0424, 42.421), "distance");
-  PlacesSearchResponse response = await places.searchByText("123 Main Street");
-
-  PlacesDetailsResponse response = await places.getDetailsByPlaceId("PLACE_ID");
-  PlacesDetailsResponse response = await places.getDetailsByReference("REF");
-   */
-
-  Future<PlacesSearchResponse> _GetNearbyLocations(double latNum, double lngNum) async {
-    final places = new GoogleMapsPlaces(apiKey: "AIzaSyAt6zT1WRtRiDwpfXwzxCnqo4ZHG18suCM");
-    return places.searchNearbyWithRadius(new Location(lat: latNum, lng: lngNum), 5000, type: "restaurant");
-  }
 
   // Nearby Restaurants
-  Future<Position> _currLocation;
   Set<Marker> _markers = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _currLocation = Geolocator.getCurrentPosition();
-  }
 
   Future<void> _retrieveNearbyRestaurants(LatLng _userLocation) async {
     PlacesSearchResponse _response = await places.searchNearbyWithRadius(
-        Location(lat: _userLocation.latitude, lng: _userLocation.longitude), 10000,
-        type: "restaurant");
+        Location(lat: _userLocation.latitude, lng: _userLocation.longitude), 100, type: "restuarant", keyword: "pizza");
 
-    Set<Marker> _restaurantMarkers = _response.results
-        .map((result) => Marker(
+    // print results
+    _response.results.forEach((element) {
+      print(element.name);
+    });
+
+    Set<Marker> _restaurantMarkers = _response.results.map((result) => Marker(
         markerId: MarkerId(result.name),
         // Use an icon with different colors to differentiate between current location
         // and the restaurants
@@ -161,95 +106,39 @@ class MapSampleState extends State<MapSample> {
         infoWindow: InfoWindow(
             title: result.name,
             snippet: "Ratings: " + (result.rating?.toString() ?? "Not Rated")),
-        position: LatLng(
-            result.geometry.location.lat, result.geometry.location.lng)))
-        .toSet();
+        position: LatLng(result.geometry.location.lat, result.geometry.location.lng))).toSet();
 
     setState(() {
       _markers.addAll(_restaurantMarkers);
     });
   }
 
-  Completer<GoogleMapController> _controller = Completer();
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: FutureBuilder(
-          future: _determinePosition(),
-          builder: (context, AsyncSnapshot<Position> currLoc) {
-            if (currLoc.hasData) {
-              double latitude = currLoc.data.latitude;
-              double longitude = currLoc.data.longitude;
-              return FutureBuilder (
-                  future: _GetNearbyLocations(latitude, longitude),
-                  builder: (context, AsyncSnapshot<PlacesSearchResponse> nearbyLoc) {
-                    if (nearbyLoc.hasData) {
-                      print(nearbyLoc.data.results);
-                      return GoogleMap(
-                        mapType: MapType.hybrid,
-                        markers: _markers,
-                        onTap: (point) {
-                          if (_isMarker) {
-                            setState(() {
-                              _markers.clear();
-                              //_setMarkers(LatLng(33.97237, -117.327469));
-                              _setMarkers(LatLng(latitude, longitude));
-                            });
-                          };
-                        },
-                        initialCameraPosition: CameraPosition(
-                          target: LatLng(latitude, longitude),
-                          //target: LatLng(33.97237, -117.327469), // hard code values
-                          zoom: 15,
-                        ),
-                        onMapCreated: (GoogleMapController controller) {
-                          _controller.complete(controller);
-                        },
-                      );
-                    }
-                    else {
-                      return Container();
-                    }
-                  }
-              );
-            }
-            else {
-              return Container();
-            }
-          }
-      ),
-    );
-  }
-  */
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _currLocation,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
+        future: _determinePosition(),
+        builder: (context, AsyncSnapshot<Position> currLoc) {
+          if (currLoc.connectionState == ConnectionState.done) {
+            if (currLoc.hasData) {
               // The user location returned from the snapshot
-              Position snapshotData = snapshot.data;
-              LatLng _userLocation =
-              LatLng(snapshotData.latitude, snapshotData.longitude);
+              double latitude = currLoc.data.latitude;
+              double longitude = currLoc.data.longitude;
 
               if (_markers.isEmpty) {
-                _retrieveNearbyRestaurants(_userLocation);
+                _retrieveNearbyRestaurants(LatLng(latitude, longitude));
               }
 
               return GoogleMap(
                 initialCameraPosition: CameraPosition(
-                  target: _userLocation,
-                  zoom: 12,
+                  target: LatLng(latitude, longitude),
+                  zoom: 15,
                 ),
                 markers: _markers
                   ..add(Marker(
                       markerId: MarkerId("User Location"),
                       infoWindow: InfoWindow(title: "User Location"),
-                      position: _userLocation)),
+                      position: LatLng(latitude, longitude))
+                  )
               );
             } else {
               return Center(child: Text("Failed to get user location."));
@@ -260,172 +149,3 @@ class MapSampleState extends State<MapSample> {
         });
   }
 }
-
-/**************************/
-
-// Find Nearby locations
-
-/**************************/
-
-
-/***********************************************/
-
-// Geolocator initial Code to test functionality
-
-/***********************************************/
-
-/*
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    /// Determine the current position of the device.
-    ///
-    /// When the location services are not enabled or permissions
-    /// are denied the `Future` will return an error.
-    Future<Position> _determinePosition() async {
-      bool serviceEnabled;
-      LocationPermission permission;
-
-      // Test if location services are enabled.
-      serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        // Location services are not enabled don't continue
-        // accessing the position and request users of the
-        // App to enable the location services.
-        return Future.error('Location services are disabled.');
-      }
-
-      permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.deniedForever) {
-          // Permissions are denied forever, handle appropriately.
-          return Future.error(
-              'Location permissions are permanently denied, we cannot request permissions.');
-        }
-
-        if (permission == LocationPermission.denied) {
-          // Permissions are denied, next time you could try
-          // requesting permissions again (this is also where
-          // Android's shouldShowRequestPermissionRationale
-          // returned true. According to Android guidelines
-          // your App should show an explanatory UI now.
-          return Future.error(
-              'Location permissions are denied');
-        }
-      }
-
-      // When we reach here, permissions are granted and we can
-      // continue accessing the position of the device.
-      return await Geolocator.getCurrentPosition();
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FutureBuilder(
-              future: _determinePosition(),
-              builder: (context, AsyncSnapshot<Position> snapshot) {
-                return Text(
-                  snapshot.data.toString(),
-                );
-              }
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
